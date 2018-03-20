7f87cdb2acfeb4bc8d15b97e0a0a7237e8488b85
santiago.gonzalez <santiago.gonzalez.courel@gmail.com>
2018-03-20 15:42:57 +0100

    change wide by fluid

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -37,7 +37,7 @@
       <!-- begin::Header -->
       <header class="m-grid__item   m-header "  data-minimize="minimize" data-minimize-offset="200" data-minimize-mobile-offset="200" >
         <div class="m-header__top">
-          <div class="m-container m-container--responsive m-container--xxl m-container--full-height m-page__container">
+          <div class="m-container m-container--fluid m-container--full-height m-page__container">
             <div class="m-stack m-stack--ver m-stack--desktop">
               <!-- begin::Brand -->
               <div class="m-stack__item m-brand">
